875c783c7ea77af7b01c4769ec49933d7d223026
Bobby <38087760+bobbygill@users.noreply.github.com>
2019-06-18 13:17:42 -0700

    Fixed spacing

--- src/App.js
+++ src/App.js
@@ -3,86 +3,86 @@ import "./App.css";
 import DeleteButton from "./DeleteButton";
 
 class App extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			shoppingItem: "",
-			list: []
-		};
+    this.state = {
+      shoppingItem: "",
+      list: []
+    };
 
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-		this.handleRemove = this.handleRemove.bind(this);
-		this.shoppingList = [];
-	}
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleRemove = this.handleRemove.bind(this);
+    this.shoppingList = [];
+  }
 
-	handleChange(e) {
-		const { name, value, checked, type } = e.target;
-		this.setState({
-			//    [name]: type === 'checkbox' ? checked : value
-			[name]: value
-		});
-	}
+  handleChange(e) {
+    const { name, value, checked, type } = e.target;
+    this.setState({
+      //    [name]: type === 'checkbox' ? checked : value
+      [name]: value
+    });
+  }
 
-	handleSubmit(e) {
+  handleSubmit(e) {
+    e.preventDefault();
+    let list2 = [...this.state.list, this.state.shoppingItem];
 
-		e.preventDefault();
-		let list2 = [...this.state.list, this.state.shoppingItem];
+    this.setState({
+      list: list2
+    });
+    this.setState({
+      shoppingItem: ""
+    });
+  }
 
-		this.setState({
-			list: list2
-		});
-		this.setState({
-			shoppingItem: ""
-		});
-	}
+  handleRemove(e) {
+    // console.log("e.target.value", e.target.value); blank
+    // console.log("e.target.id", e.target.id); apple
+    // console.log(identifier);
 
-	handleRemove(e) {
-		// console.log("e.target.value", e.target.value); blank
-		// console.log("e.target.id", e.target.id); apple
-		// console.log(identifier);
+    //if you add duplicate items to the list and hit delete, both are removed,
+    // need to try adding item name + index together to create a unique id
+    let newList = this.state.list.filter(x => e.target.id != x);
+    // let newList = this.state.list.filter(x => console.log(this.state.list.indexOf(x).toString()) + console.log(x.id) != identifier);
+    this.setState({ list: newList });
+  }
 
-		//if you add duplicate items to the list and hit delete, both are removed, need to try adding item name + index together to create a unique id
-		let newList = this.state.list.filter(x => e.target.id != x);
-		// let newList = this.state.list.filter(x => console.log(this.state.list.indexOf(x).toString()) + console.log(x.id) != identifier);
-		this.setState({ list: newList });
-	}
+  render() {
+    return (
+      <div>
+        <form className="App" onSubmit={this.handleSubmit}>
+          <input
+            name="shoppingItem"
+            value={this.state.shoppingItem}
+            placeholder="Shopping Item"
+            onChange={this.handleChange}
+          />
+          <button type="submit">Add to Shopping list</button>
+        </form>
 
-	render() {
-		return (
-			<div>
-				<form className="App" onSubmit={this.handleSubmit}>
-					<input
-						name="shoppingItem"
-						value={this.state.shoppingItem}
-						placeholder="Shopping Item"
-						onChange={this.handleChange}
-					/>
-					<button type="submit">Add to Shopping list</button>
-				</form>
+        <ul>
+          {this.state.list.map((item, index) => {
+            if (item == "") {
+              return;
+            }
+            return (
+              <div>
+                <li key={item}>{item} </li>
 
-				<ul>
-					{this.state.list.map((item, index) => {
-						if (item == "") {
-							return;
-						}
-						return (
-							<div>
-								<li key={item}>{item} </li>
-
-								<DeleteButton
-									id={item}
-									index={index}
-									onClick={this.handleRemove}
-								/>
-							</div>
-						);
-					})}
-				</ul>
-			</div>
-		);
-	}
+                <DeleteButton
+                  id={item}
+                  index={index}
+                  onClick={this.handleRemove}
+                />
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/DeleteButton.js
+++ src/DeleteButton.js
@@ -1,23 +1,21 @@
 import React from "react";
 
-function DeleteButton(props){
-
-//maybe I can pass the index with the id to create a unique ID
-// const handleClick = () => {
-//    props.onClick(`${props.index}${props.id}`);
-//  }
-
-return (
-  <button
-    id={props.id}
-    // onClick={handleClick}
-    onClick={props.onClick}
-    index={props.index}
-  >
-    delete
-  </button>
-);
-
+function DeleteButton(props) {
+  //maybe I can pass the index with the id to create a unique ID
+  // const handleClick = () => {
+  //    props.onClick(`${props.index}${props.id}`);
+  //  }
+  //
+  return (
+    <button
+      id={props.id}
+      // onClick={handleClick}
+      onClick={props.onClick}
+      index={props.index}
+    >
+      delete
+    </button>
+  );
 }
 
 export default DeleteButton;
